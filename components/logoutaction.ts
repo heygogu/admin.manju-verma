6cd382f953265a7c3a355f6888353f8162103f0d
heygogu <rohitnarnolia88@gmail.com>
2025-03-11 00:17:58 +0530

    bug fixes

--- components/logoutaction.ts
+++ components/logoutaction.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 
 export async function LogoutAction() {
   try {
-    const response= await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/api/logout`);
+    const response= await axios.post(`${process.env.NEXT_PUBLIC_BASE_URL}/api/logout`);
     console.log(response);
    
     return {success:true}
