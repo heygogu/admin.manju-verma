229a803d3c961c9e9720bfefc5c989bd33213bf3
Rohit <rohit.henceforth@gmail.com>
2025-03-11 13:02:42 +0530

    random commit

--- lib/cloudinary.ts
+++ lib/cloudinary.ts
@@ -1,8 +1,5 @@
 import { v2 as cloudinary } from "cloudinary";
 
-// NEXT_PUBLIC_CLOUDINARY_KEY=215247942342647
-// NEXT_PUBLIC_CLOUDINARY_SECRET=ra5FBb_BwJq8mO1OKdu7pd_wfmE
-// NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME=dmorrbtjy
 cloudinary.config({
   cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
   api_key: process.env.NEXT_PUBLIC_CLOUDINARY_KEY,
